474685842fd2d93294a2de3eb2ac94c336cefc9f
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-04 13:36:41 +0100

    add select_observed_range()

---
+++ R/select_observed_range.R
@@ -0,0 +1,23 @@
+#' Limit the observation to the range in which the species is present
+#' @inheritParams select_factor_threshold
+#' @export
+#' @examples
+#' observation <- data.frame(
+#'   Count = c(0, 0, 100, 101, 0, 51, 1, 0, 0, 0),
+#'   Year = 1:10
+#' )
+#' select_observed_range(observation, "Year")
+select_observed_range <- function(observation, variable){
+  variable <- check_character(x = variable, name = "variable", na.action = na.fail)
+  junk <- check_dataframe_variable(
+    df = observation, variable = c("Count", variable), name = "observation", error = TRUE
+  )
+  if(any(is.na(observation[, variable]))){
+    warning(variable, " contains missing values. Corresponding rows are removed.")
+  }
+  
+  observed.range <- range(observation[observation$Count > 0, variable], na.rm = TRUE)
+  selection <- observed.range[1] <= observation[, variable] & observation[, variable] <= observed.range[2]
+  return(observation[selection, ])
+}
+
