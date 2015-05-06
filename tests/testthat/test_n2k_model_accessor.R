f3a0e091097a7c0b76ef77e04ed803f6f748bdea
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-06 14:39:04 +0200

    add status() accessor function for n2kModel objects

---
+++ tests/testthat/test_n2k_model_accessor.R
@@ -0,0 +1,38 @@
+context("n2kModel accesor functions")
+
+context("status() handles n2kModel objects")
+describe("status", {
+  data("cbpp", package = "lme4")
+  object <- n2k_glmer_poisson(
+    data = cbpp
+  )
+  model.object <- lme4::glmer(
+    incidence ~ offset(log(size)) + period + (1 | herd), 
+    data = object@Data,
+    family = poisson
+  )
+  object.model <- n2k_glmer_poisson(
+    data = object, model.fit = model.object, status = "converged"
+  )
+  it("returns the status", {
+    expect_that(
+      status(object),
+      is_identical_to(object@Status)
+    )
+    expect_that(
+      status(object.model),
+      is_identical_to(object.model@Status)
+    )
+  })
+  it("updates the status", {
+    status(object) <- "error"
+    expect_that(
+      status(object),
+      is_identical_to("error")
+    )
+    expect_that(
+      status(object) <- "junk",
+      throws_error("Status must be one of the following")
+    )
+  })
+})
