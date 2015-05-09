de3912c2a8009add1ebced20a7e44b6ef16ed4e5
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-09 21:37:59 +0200

    create a fit_model() function that handles n2kModel objects

--- R/fit_every_model.R
+++ R/fit_every_model.R
@@ -1,16 +1,9 @@
 #' Fit the model to the analysis files
-#' @inheritParams fit_single_model
+#' @param path the path containing the analysis files
 #' @export
 fit_every_model <- function(path = "."){
-  if(!file_test("-d", path)){
-    stop(path, " is not a directory")
-  }
-  files <- list.files(path = path, pattern = "\\.rda$")
-  convergence <- lapply(files, fit_single_model, path = path)
-  convergence <- do.call(rbind, convergence)
-  if(!file_test("-d", paste0(path, "/database"))){
-    dir.create(paste0(path, "/database"))
-  }
-  save(convergence, file = paste0(path, "/database/convergence.rda"))
-  return(convergence)
+  path <- check_path(path, type = "directory")
+  files <- list.files(path = path, pattern = "\\.rda$", full.names = TRUE)
+  junk <- lapply(files, fit_model)
+  return(invisible(NULL))
 }
---
+++ R/fit_model.R
@@ -0,0 +1,135 @@
+#' Fit a n2kModel object
+#' @param x the n2kModel
+#' @name fit_model
+#' @rdname fit_model
+#' @exportMethod fit_model
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "fit_model", 
+  def = function(x){
+    standard.generic("fit_model")
+  }
+)
+
+#' @rdname fit_model
+#' @importFrom methods setMethod
+#' @importFrom n2khelper check_path read_object_environment
+setMethod(
+  f = "fit_model",
+  signature = signature(x = "character"),
+  definition = function(x){
+    x <- check_path(x, type = "file")
+    local.environment <- new.env()
+    load(x, envir = local.environment)
+    analysis <- read_object_environment(object = "analysis", env = local.environment)
+    analysis <- tryCatch(
+      fit_model(x = analysis),
+      error = function(e){
+        warning(e)
+      } 
+    )
+    if(inherits(x, what = "n2kModel")){
+      save(analysis, file = x)
+    }
+    return(invisible(NULL))
+  }
+)
+
+#' @rdname fit_model
+#' @importFrom methods setMethod
+#' @importFrom n2khelper check_dataframe_covariate check_dataframe_variable
+#' @include n2kGlmerPoisson_class.R
+setMethod(
+  f = "fit_model",
+  signature = signature(x = "n2kGlmerPoisson"),
+  definition = function(x){
+    if(status(x) != "new"){
+      return(x)
+    }
+    
+    data <- get_data(x)
+    check_dataframe_covariate(
+      df = data[1, ], covariate = get_covariate(x), response = "Count", error = TRUE
+    )  
+    
+    weight <- get_weight(x)
+    if(weight == ""){
+      local.weight <- rep(1, nrow(data))
+    } else {
+      check_dataframe_variable(df = data, variable = weight, name = "data")
+      local.weight <- data[, weight]
+    }
+    
+    model.formula <- as.formula(paste("Count ~", get_covariate(x)))
+    controls <- list(
+      lme4::glmerControl(optimizer = "bobyqa"),
+      lme4::glmerControl(optimizer = "optimx", optCtrl = list(method = "nlminb"))
+    )
+    for(control in controls){
+      if("optimx" %in% control$optimizer){
+        requireNamespace("optimx", quietly = TRUE)
+      }
+      model <- try(lme4::glmer(
+        formula = model.formula,
+        data = data,
+        family = "poisson",
+        weights = local.weight,
+        control = control
+      ))
+      if("try-error" %in% class(model)){
+        next
+      }
+      if(length(model@optinfo$conv$lme4) == 0){
+        break
+      }
+    }
+    if("try-error" %in% class(model)){
+      status(x) <- "error"
+      return(x)
+    }
+    if(length(model@optinfo$conv$lme4) > 0){
+      status(x) <- "false convergence"
+      return(x)
+    }
+    return(n2k_glmer_poisson(data = x, model.fit = model, status = "converged"))
+  }
+)
+
+#' @rdname fit_model
+#' @importFrom methods setMethod
+#' @importFrom n2khelper check_dataframe_covariate
+#' @include n2kInlaNbinomial_class.R
+setMethod(
+  f = "fit_model",
+  signature = signature(x = "n2kInlaNbinomial"),
+  definition = function(x){
+    if(!requireNamespace("INLA", quietly = TRUE)){
+      stop("The INLA package is required but not installed.")
+    }
+    
+    data <- get_data(x)
+    check_dataframe_covariate(
+      df = data[1, ], covariate = get_covariate(x), response = "Count", error = TRUE
+    )  
+    
+    form <- as.formula(paste("Count ~", get_covariate(x)))
+    link <- rep(NA, nrow(data))
+    link[is.na(data$Count)] <- 1
+    
+    model <- try(INLA::inla(
+      formula = form, 
+      family = "nbinomial", 
+      data = data, 
+      control.compute = list(dic = TRUE, cpo = TRUE),
+      control.predictor = list(compute = TRUE, link = link),
+      control.fixed = list(prec.intercept = 1)
+    ))
+    if("try-error" %in% class(model)){
+      status(x) <- "error"
+      return(x)
+    }
+    return(n2k_inla_nbinomial(data = x, model.fit = model, status = "converged"))
+  }
+)
+
