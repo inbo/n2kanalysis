c9d27529c15580ff3ef30254b47956e2e097c95a
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-17 21:47:07 +0200

    allow the "working" and "waiting" status in the n2kModel class

--- R/n2kModel_class.R
+++ R/n2kModel_class.R
@@ -68,7 +68,7 @@ setValidity(
     check_single_character(object@Covariate, name = "Covariate")
     check_single_posix(object@AnalysisDate, name = "AnalysisDate", past = TRUE)
     
-    ok.status <- c("new", "error", "converged", "false convergence", "insufficient data")
+    ok.status <- c("new", "working", "waiting", "error", "converged", "false convergence", "insufficient data")
     if(!object@Status %in% ok.status){
       stop(
         "Status must be one of the following: ", 
