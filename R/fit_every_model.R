ae3e9d082fc314caba65bace1a4ca36791a1486c
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-27 13:44:44 +0200

    fit the models in parallel

--- R/fit_every_model.R
+++ R/fit_every_model.R
@@ -2,8 +2,9 @@
 #' @param path the path containing the analysis files
 #' @param status a vector with status levels to (re-)fit the model. Defaults to c("new", "waiting") when missing.
 #' @param verbose Show the name of the current analysis file on screen. Defaults to TRUE
+#' @param n.cluster the number of clusters to use
 #' @export
-fit_every_model <- function(path = ".", status, verbose = TRUE){
+fit_every_model <- function(path = ".", status, verbose = TRUE, n.cluster = 1){
   if(missing(status)){
     status <- c("new", "waiting")
   } else {
@@ -19,6 +20,35 @@ fit_every_model <- function(path = ".", status, verbose = TRUE){
   }
   path <- check_path(path, type = "directory")
   files <- list.files(path = path, pattern = "\\.rda$", full.names = TRUE)
-  junk <- lapply(files, fit_model, status = status, verbose = verbose)
+  if(n.cluster == 1){
+    junk <- lapply(files, fit_model, status = status, verbose = verbose)
+  } else {
+    if(requireNamespace("parallel", quietly = TRUE)){
+      available.cluster <- parallel::detectCores()
+      if(n.cluster > available.cluster){
+        message("Requesting ", n.cluster, " clusters but only ", available.cluster, " available.")
+        n.cluster <- available.cluster
+      }
+      message("Fitting models in parallel on ", n.cluster, " clusters")
+      utils::flush.console()
+      cl <- parallel::makeCluster(n.cluster)
+      result <- parallel::clusterApplyLB(
+        cl = cl, 
+        x = files, 
+        fun = function(x, status, verbose){
+          require(n2kanalysis)
+          fit_model(x = x, status = status, verbose = verbose)
+        }, 
+        status = status, 
+        verbose = verbose
+      )
+      parallel::stopCluster(cl)
+    } else {
+      message("Cannot load the parallel package. Falling back to non-parallel computing.")
+      utils::flush.console()
+      junk <- lapply(files, fit_model, status = status, verbose = verbose)
+    }
+  }
+  
   return(invisible(NULL))
 }
