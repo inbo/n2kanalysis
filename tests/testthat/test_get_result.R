2235a798f111d2823753056214ab748fc9494d62
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-10-05 13:45:09 +0200

    add unit tests for the conversion of fingerprints to id by get_result()

--- tests/testthat/test_get_result.R
+++ tests/testthat/test_get_result.R
@@ -189,18 +189,203 @@ expect_message(
     parallel::detectCores(), "available."
   )
 )
-result <- get_result(
+result.fp <- get_result(
   temp.dir,
   datasource.id = this.datasource,
   keep.fingerprint = TRUE
 )
-expect_is(result, "n2kResult")
-result <- get_result(
+expect_is(result.fp, "n2kResult")
+result.id <- get_result(
   temp.dir,
   datasource.id = this.datasource,
   keep.fingerprint = FALSE
 )
-expect_is(result, "n2kResult")
+expect_is(result.id, "n2kResult")
+
+# check the convertion for fingerprint to ID
+analysis.level <- sort(unique(c(
+  result.fp@AnalysisMetadata$FileFingerprint,
+  result.fp@AnalysisRelation$ParentAnalysis
+)))
+analysis.id <- data_frame(
+  Analysis = result.fp@AnalysisMetadata$FileFingerprint,
+  Fingerprint = factor(
+    result.fp@AnalysisMetadata$FileFingerprint,
+    levels = analysis.level
+  ),
+  AnalysisID = result.id@AnalysisMetadata$FileFingerprint
+)
+expect_identical(
+  analysis.id$Fingerprint,
+  analysis.id$AnalysisID
+)
+analysis.id$Fingerprint <- NULL
+
+require(dplyr, quietly = TRUE)
+parameter.id <- result.fp@Parameter %>%
+  as.tbl() %>%
+  mutate_(ID = ~as.integer(factor(Fingerprint)))
+parameter.id <- parameter.id %>%
+  left_join(
+    parameter.id %>%
+      select_(~Fingerprint, ParentID = ~ID),
+    by = c("Parent" = "Fingerprint")
+  ) %>%
+  select_(~-Parent)
+aj.parameter <- parameter.id %>%
+  anti_join(
+    result.id@Parameter,
+    by = c("Description", "ID" = "Fingerprint", "ParentID" = "Parent")
+  )
+expect_identical(nrow(aj.parameter), 0L)
+
+parameter.estimate <- result.fp@ParameterEstimate %>%
+  inner_join(analysis.id, by = "Analysis") %>%
+  as.tbl() %>%
+  select_(~AnalysisID, ~Parameter) %>%
+  inner_join(
+    parameter.id %>% select_(~Fingerprint, ParameterID = ~ID),
+    by = c("Parameter" = "Fingerprint")
+  ) %>%
+  select_(~-Parameter)
+aj.parameterestimate <- parameter.estimate %>%
+  anti_join(
+    result.id@ParameterEstimate,
+    by = c("AnalysisID" = "Analysis", "ParameterID" = "Parameter")
+  )
+expect_identical(nrow(aj.parameterestimate), 0L)
+
+contrast <- result.fp@Contrast %>%
+  as.tbl() %>%
+  inner_join(analysis.id, by = "Analysis") %>%
+  select_(~-Analysis) %>%
+  mutate_(ContrastID = ~as.integer(factor(Fingerprint)))
+aj.contrast <- contrast %>%
+  anti_join(
+    result.id@Contrast,
+    by = c("ContrastID" = "Fingerprint", "Description", "AnalysisID" = "Analysis")
+  )
+expect_identical(nrow(aj.contrast), 0L)
+
+aj.contrastcoefficient <- result.fp@ContrastCoefficient %>%
+  as.tbl() %>%
+  inner_join(
+    parameter.id %>%
+      select_(Parameter = ~Fingerprint, ParameterID = ~ID),
+    by = "Parameter"
+  ) %>%
+  select_(~-Parameter) %>%
+  inner_join(
+    contrast %>%
+      select_(~Fingerprint, ~ContrastID),
+    by = c("Contrast" = "Fingerprint")
+  ) %>%
+  anti_join(
+    result.id@ContrastCoefficient,
+    by = c("ContrastID" = "Contrast", "ParameterID" = "Parameter", "Coefficient")
+  )
+expect_identical(nrow(aj.contrastcoefficient), 0L)
+
+aj.contrastestimate <- result.fp@ContrastEstimate %>%
+  inner_join(
+    contrast %>%
+      select_(~Fingerprint, ~ContrastID),
+    by = c("Contrast" = "Fingerprint")
+  ) %>%
+  select_(~-Contrast) %>%
+  anti_join(
+    result.id@ContrastEstimate,
+    by = c(
+      "ContrastID" = "Contrast",
+      "Estimate",
+      "LowerConfidenceLimit",
+      "UpperConfidenceLimit"
+    )
+  )
+expect_identical(nrow(aj.contrastestimate), 0L)
+
+anomalytype <- result.fp@AnomalyType %>%
+  mutate_(ID = ~as.integer(factor(Fingerprint)))
+aj.anomalytype <- anomalytype %>%
+  anti_join(result.id@AnomalyType, by = c("Description", "ID" = "Fingerprint"))
+expect_identical(nrow(aj.anomalytype), 0L)
+
+aj.anomaly <- result.fp@Anomaly %>%
+  as.tbl() %>%
+  inner_join(analysis.id, by = "Analysis") %>%
+  select_(~-Analysis) %>%
+  inner_join(
+    anomalytype %>%
+      select_(~Fingerprint, AnomalyTypeID = ~ID),
+    by = c("AnomalyType" = "Fingerprint")
+  ) %>%
+  select_(~-AnomalyType) %>%
+  inner_join(
+    parameter.id %>%
+      select_(~Fingerprint, ParameterID = ~ID),
+    by = c("Parameter" = "Fingerprint")
+  ) %>%
+  select_(~-Parameter) %>%
+  mutate_(Datafield = ~factor(Datafield)) %>%
+  anti_join(
+    result.id@Anomaly,
+    by = c(
+      "AnomalyTypeID" = "AnomalyType", "AnalysisID" = "Analysis",
+      "ParameterID" = "Parameter", "DatasourceID", "Estimate", "Datafield")
+  )
+expect_identical(nrow(aj.anomaly), 0L)
+
+analysisversion <- data_frame(
+  AnalysisVersion = result.fp@AnalysisVersion$Fingerprint,
+  AnalysisVersionID = result.id@AnalysisVersion$Fingerprint,
+  Fingerprint = factor(result.fp@AnalysisVersion$Fingerprint)
+)
+expect_identical(analysisversion$AnalysisVersionID, analysisversion$Fingerprint)
+
+rpackage <- result.fp@RPackage %>%
+  as.tbl() %>%
+  mutate_(RPackageID = ~as.integer(factor(Fingerprint)))
+aj.rpackage <- rpackage %>%
+  anti_join(
+    result.id@RPackage,
+    by = c("Description", "Version", "RPackageID" = "Fingerprint")
+  )
+expect_identical(nrow(aj.rpackage), 0L)
+
+aj.analysisrpackage <- result.fp@AnalysisVersionRPackage %>%
+  as.tbl() %>%
+  inner_join(analysisversion, by = "AnalysisVersion") %>%
+  select_(AnalysisVersion = ~AnalysisVersionID, ~RPackage) %>%
+  inner_join(
+    rpackage %>%
+      select_(RPackage = ~Fingerprint, ~RPackageID),
+    by = c("RPackage")
+  ) %>%
+  anti_join(
+    result.id@AnalysisVersionRPackage,
+    by = c("AnalysisVersion", "RPackageID" = "RPackage")
+  )
+expect_identical(nrow(aj.analysisrpackage), 0L)
+
+aj.analysisparent <- result.fp@AnalysisRelation %>%
+  as.tbl() %>%
+  inner_join(analysis.id, by = "Analysis") %>%
+  select_(~-Analysis) %>%
+  mutate_(
+    ParentAnalysis = ~factor(ParentAnalysis, levels = analysis.level),
+    ParentStatusFingerprint = ~factor(ParentStatusFingerprint),
+    ParentStatus = ~factor(ParentStatus)
+  ) %>%
+  anti_join(
+    result.id@AnalysisRelation,
+    by = c(
+      "AnalysisID" = "Analysis",
+      "ParentAnalysis",
+      "ParentStatusFingerprint",
+      "ParentStatus"
+    )
+  )
+expect_identical(nrow(aj.analysisparent), 0L)
 
 # clean temp files
 file.remove(
