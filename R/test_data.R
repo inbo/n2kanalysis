3a27f45587d934e8fc6f2b9c041e1fbac1255197
ThierryO <Thierry.Onkelinx@inbo.be>
2016-06-19 20:51:34 +0200

    add unit test for get_model_parameters() on n2kInlaNbinomial

---
+++ R/test_data.R
@@ -0,0 +1,41 @@
+# A function to generate a simple dataset for unit testing
+#' @importFrom dplyr %>%
+#' @importFrom stats model.matrix
+test_data <- function(){
+  set.seed(999)
+  n.e <- 10
+  sd.random <- 0.1
+  theta <- 4
+
+  dataset <- expand.grid(
+    A = factor(c("a1", "a2", "a3")),
+    B = factor(c("b1", "b2")),
+    C = 1:3,
+    D = -3:-1,
+    E = seq_len(n.e),
+    F = seq_len(3)
+  )
+  mm.fixed <- model.matrix(
+    ~ A * (B + C) + C * D,
+    data = dataset
+  )
+  fixed <- runif(ncol(mm.fixed))
+
+  mm.random <- model.matrix(
+    ~ 0 + factor(E) : A,
+    data = dataset
+  )
+  random <- dataset$A %>%
+    levels() %>%
+    length() %>%
+    "*"(n.e) %>%
+    rnorm(sd = sd.random) %>%
+    matrix(nrow = n.e) %>%
+    apply(2, cumsum) %>%
+    as.vector()
+  eta <- mm.fixed %*% fixed + mm.random %*% random
+  dataset$Count <- rnbinom(nrow(dataset), mu = exp(eta), size = theta)
+  dataset$DatasourceID <- 1
+  dataset$ObservationID <- seq_along(dataset$Count)
+  return(dataset)
+}
--- tests/testthat/test_get_model_parameter.R
+++ tests/testthat/test_get_model_parameter.R
@@ -127,3 +127,95 @@ test_that(
   )
   expect_true("size:period" %in% param@Parameter$Description)
 })
+
+test_that("get_model_parameter works with n2kInlaNbinomial", {
+  dataset <- n2kanalysis:::test_data()
+  this.analysis.date <- as.POSIXct("2015-01-01 04:05:06.12", tz = "UTC")
+  this.scheme.id <- 1L
+  this.species.group.id <- 2L
+  this.location.group.id <- 3L
+  this.seed <- 4L
+  this.model.type <- "inla nbinomial: A * B + E"
+  this.first.imported.year <- 1990L
+  this.last.imported.year <- 2015L
+  this.last.analysed.year <- 2015L
+  this.parent <- "abcdef"
+  this.duration <- this.last.imported.year - this.first.imported.year + 1
+  analysis <- n2k_inla_nbinomial(
+    formula = "Count ~ A + C",
+    scheme.id = this.scheme.id,
+    species.group.id = this.species.group.id,
+    location.group.id = this.location.group.id,
+    model.type = this.model.type,
+    first.imported.year = this.first.imported.year,
+    last.imported.year = this.last.imported.year,
+    last.analysed.year = this.last.analysed.year,
+    analysis.date = this.analysis.date,
+    seed = this.seed,
+    data = dataset,
+    parent = this.parent,
+    this.duration
+  )
+  expect_is(
+    param <- get_model_parameter(analysis),
+    "n2kParameter"
+  )
+  expect_identical(nrow(param@Parameter), 0L)
+  expect_identical(nrow(param@ParameterEstimate), 0L)
+
+  analysis <- fit_model(analysis)
+  expect_message(
+    param <- get_model_parameter(analysis, verbose = TRUE),
+    "reading model parameters: fixed effects"
+  )
+  expect_is(param, "n2kParameter")
+  expect_identical(
+    param@Parameter %>%
+      semi_join(
+        data_frame(Description = "Random effect BLUP"),
+        by = "Description"
+      ) %>%
+      inner_join(
+        param@Parameter,
+        by = c("Fingerprint" = "Parent")
+      ) %>%
+     nrow(),
+    0L
+  )
+  expect_identical(
+    param@Parameter %>%
+      semi_join(
+        data_frame(Description = "Random effect variance"),
+        by = "Description"
+      ) %>%
+      inner_join(
+        param@Parameter,
+        by = c("Fingerprint" = "Parent")
+      ) %>%
+     nrow(),
+    0L
+  )
+  fixed <- param@Parameter %>%
+    semi_join(
+    param@Parameter %>%
+      semi_join(
+        data_frame(Description = "Fixed effect"),
+        by = "Description"
+      ),
+    by = c("Parent" = "Fingerprint")
+  ) %>%
+    select_(~Fingerprint, Main = ~Description) %>%
+    left_join(
+      param@Parameter,
+      by = c("Fingerprint" = "Parent")
+    )
+  expect_identical(nrow(fixed), 4L)
+  expect_identical(
+    fixed$Main == "A",
+    !is.na(fixed$Description)
+  )
+  expect_identical(
+    fixed$Main == "A",
+    !is.na(fixed$Fingerprint.y)
+  )
+})
