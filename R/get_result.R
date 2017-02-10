ad81fdf621dd2372ed6e2b476241e8547e478638
ThierryO <Thierry.Onkelinx@inbo.be>
2017-02-10 10:37:52 +0100

    get_result() works on s3_objects

--- R/get_result.R
+++ R/get_result.R
@@ -405,3 +405,15 @@ setMethod(
     return(result)
   }
 )
+
+#' @rdname get_result
+#' @importFrom methods setMethod new
+#' @include import_S3_classes.R
+setMethod(
+  f = "get_result",
+  signature = signature(x = "s3_object"),
+  definition = function(x, ...){
+    x <- s3readRDS(object = x)
+    get_result(x, ...)
+  }
+)
