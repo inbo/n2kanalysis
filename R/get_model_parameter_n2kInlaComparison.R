9776b6034f8e82d1ad363aca09a4f4a9c563f25d
ThierryO <Thierry.Onkelinx@inbo.be>
2017-07-27 11:04:05 +0200

    split get_model_parameter.R over several files

---
+++ R/get_model_parameter_n2kInlaComparison.R
@@ -0,0 +1,12 @@
+#' @rdname get_model_parameter
+#' @importFrom methods setMethod new
+#' @include n2kInlaComparison_class.R
+#' @include n2kParameter_class.R
+setMethod(
+  f = "get_model_parameter",
+  signature = signature(analysis = "n2kInlaComparison"),
+  definition = function(analysis, ...){
+    warning("reading model parameters on n2kInlaComparison is to do")
+    return(new("n2kParameter"))
+  }
+)
