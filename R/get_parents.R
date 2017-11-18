3a0a60543577ac0d8dc4716b0a5ce3ffca7102a1
ThierryO <Thierry.Onkelinx@inbo.be>
2017-11-19 00:04:24 +0100

    add get_parents()

---
+++ R/get_parents.R
@@ -0,0 +1,14 @@
+#' Get the object of the parent analyses of a n2kModel object
+#' @param child the child object
+#' @inheritParams read_model
+#' @importFrom assertthat assert_that
+#' @export
+get_parents <- function(child, base, project) {
+  assert_that(inherits(child, "n2kModel"))
+  lapply(
+    child@AnalysisRelation$ParentAnalysis,
+    read_model,
+    base = base,
+    project = project
+  )
+}
