2465f9d02e846d0091ead1bc0ca9e0a895439c83
ThierryO <Thierry.Onkelinx@inbo.be>
2017-11-27 14:55:01 +0100

    add unit test for store_manifest_yaml() on a file system

--- tests/testthat/test_bbb_store_manifest_yaml.R
+++ tests/testthat/test_bbb_store_manifest_yaml.R
@@ -68,3 +68,48 @@ test_that("store_manifest_yaml stores the manifest on an S3 bucket", {
   )
   expect_true(all(sapply(available, delete_object, bucket = bucket)))
 })
+
+test_that("store_manifest_yaml stores the manifest on a file system", {
+  base <- tempdir()
+  object <- n2k_manifest(
+    data.frame(
+      Fingerprint = "1",
+      Parent = NA_character_,
+      stringsAsFactors = FALSE
+    )
+  )
+  project <- "unittest_store_manifest_yaml"
+  docker <- "inbobmk/rn2k:latest"
+  dependencies <- c("inbo/n2khelper@v0.4.1", "inbo/n2kanalysis@docker")
+  expect_is(
+    stored <- store_manifest_yaml(
+      x = object,
+      base = base,
+      project = project,
+      docker = docker,
+      dependencies = dependencies
+    ),
+    "character"
+  )
+  expect_true(file_test("-f", stored))
+  expect_equivalent(
+    object,
+    read_manifest(base, project, object@Fingerprint)
+  )
+
+  expect_is(
+    stored2 <- store_manifest_yaml(
+      x = object,
+      base = base,
+      project = project,
+      docker = docker,
+      dependencies = dependencies
+    ),
+    "character"
+  )
+  expect_identical(
+    stored2,
+    stored
+  )
+  file.remove(stored)
+})
