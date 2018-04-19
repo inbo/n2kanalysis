e9f2d9ebea318169e741dec48f4c97b1abb1f061
ThierryO <Thierry.Onkelinx@inbo.be>
2018-04-19 15:39:11 +0200

    fit_model() downloads relevant objects
    in case fit_model() is applied on a n2kManifest() from an S3 bucket, it downloads all objects from the manifests, fits them locally and then uploads the results. This avoids downloading the same object several times.

--- R/fit_model_n2kManifest.R
+++ R/fit_model_n2kManifest.R
@@ -1,9 +1,10 @@
 #' @rdname fit_model
 #' @importFrom methods setMethod new
 #' @importFrom assertthat assert_that is.string is.flag noNA is.dir
-#' @importFrom dplyr %>% transmute_ mutate_ left_join arrange_
+#' @importFrom dplyr %>% transmute mutate left_join arrange
 #' @importFrom aws.s3 get_bucket
 #' @importFrom tibble rowid_to_column
+#' @importFrom rlang .data
 #' @include n2kManifest_class.R
 setMethod(
   f = "fit_model",
@@ -18,14 +19,14 @@ setMethod(
     }
     assert_that(is.string(dots$project))
     manifest <- x@Manifest %>%
-      mutate_(Level = ~ifelse(is.na(Parent), 0, 1))
+      mutate(Level = ifelse(is.na(.data$Parent), 0, 1))
     while (!all(is.na(manifest$Parent))) {
       manifest <- manifest %>%
         left_join(x@Manifest, by = c("Parent" = "Fingerprint")) %>%
-        transmute_(
-          ~Fingerprint,
-          Parent = ~Parent.y,
-          Level = ~ifelse(is.na(Parent), Level, Level + 1)
+        transmute(
+          .data$Fingerprint,
+          Parent = .data$Parent.y,
+          Level = ifelse(is.na(.data$Parent), .data$Level, .data$Level + 1)
         )
     }
     if (inherits(dots$base, "character")) {
@@ -39,33 +40,59 @@ setMethod(
           list.files(recursive = TRUE, full.names = TRUE),
         stringsAsFactors = FALSE
       ) %>%
-        mutate_(
-          Fingerprint = ~gsub(".*([[:xdigit:]]{40})\\.rds$", "\\1", Filename)
+        mutate(
+          Fingerprint = gsub(
+            ".*([[:xdigit:]]{40})\\.rds$",
+            "\\1",
+            .data$Filename
+          )
         ) %>%
         left_join(x = manifest, by = "Fingerprint") %>%
-        arrange_(~Level, ~Fingerprint)
+        arrange(.data$Level, .data$Fingerprint)
       sapply(manifest$Filename, fit_model, ...)
       return(invisible(NULL))
     }
-    if (inherits(dots$base, "s3_bucket")) {
-      available <- get_bucket(
-        bucket = dots$base,
-        prefix = dots$project,
-        max = Inf
-      )
-      manifest <- data.frame(
-        Filename = sapply(available, "[[", "Key"),
-        stringsAsFactors = FALSE
-      ) %>%
-        rowid_to_column() %>%
-        mutate_(
-          Fingerprint = ~gsub(".*([[:xdigit:]]{40})\\.rds$", "\\1", Filename)
-        ) %>%
-        inner_join(x = manifest, by = "Fingerprint") %>%
-        arrange_(~Level, ~Fingerprint)
-      sapply(available[manifest$rowid], fit_model, ...)
-      return(invisible(NULL))
+    if (!inherits(dots$base, "s3_bucket")) {
+      stop("base should be either a local directory or an S3 bucket")
     }
-    stop("base should be either a local directory or an S3 bucket")
+    available <- get_bucket(
+      bucket = dots$base,
+      prefix = dots$project,
+      max = Inf
+    )
+    manifest <- data.frame(
+      Filename = sapply(available, "[[", "Key"),
+      stringsAsFactors = FALSE
+    ) %>%
+      rowid_to_column() %>%
+      mutate(
+        Fingerprint = gsub(".*([[:xdigit:]]{40})\\.rds$", "\\1", .data$Filename)
+      ) %>%
+      inner_join(x = manifest, by = "Fingerprint") %>%
+      arrange(.data$Level, .data$Fingerprint)
+    if (is.null(dots$local)) {
+      dots$local <- tempdir()
+    }
+    targets <- paste(dots$local, manifest$Filename, sep = "/") %>%
+      normalizePath(mustWork = FALSE)
+    downloads <- which(!file.exists(targets))
+    for (i in downloads) {
+      if (!dir.exists(dirname(targets[i]))) {
+        dir.create(dirname(targets[i]), recursive = TRUE)
+      }
+      s3readRDS(manifest$Filename[i], bucket = dots$base) %>%
+        saveRDS(targets[i])
+    }
+    sapply(targets, fit_model, base = dots$local, project = dots$project, ...)
+    targets <- sapply(
+      basename(targets),
+      function(x) {
+        read_model(x, base = dots$local, project = dots$project) %>%
+          store_model(base = dots$base, project = dots$project)
+      }
+    )
+    paste(dots$local, targets, sep = "/") %>%
+      unlink()
+    return(invisible(NULL))
   }
 )
