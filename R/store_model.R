16a9c21a3a33530e133724fc5d0ceb8b86729c0a
ThierryO <thierry.onkelinx@inbo.be>
2018-10-25 14:04:11 +0200

    make store_model() more efficient

--- R/store_model.R
+++ R/store_model.R
@@ -65,8 +65,8 @@ setMethod(
 
 #' @rdname store_model
 #' @importFrom methods setMethod new
-#' @importFrom assertthat assert_that is.string
-#' @importFrom aws.s3 bucket_exists get_bucket s3saveRDS delete_object
+#' @importFrom assertthat assert_that is.string is.flag
+#' @importFrom aws.s3 bucket_exists get_bucket s3saveRDS delete_object copy_object
 #' @include import_S3_classes.R
 setMethod(
   f = "store_model",
@@ -74,58 +74,39 @@ setMethod(
   definition = function(x, base, project, overwrite = TRUE){
     assert_that(inherits(x, "n2kModel"))
     assert_that(is.string(project))
+    assert_that(is.flag(overwrite))
     validObject(x, complete = TRUE)
-
-    # try several times to connect to S3 bucket
-    # avoids errors due to time out
-    i <- 1
-    repeat {
-      bucket_ok <- tryCatch(
-        bucket_exists(base),
-        error = function(err) {
-          err
-        }
-      )
-      if (is.logical(bucket_ok)) {
-        break
-      }
-      if (i > 10) {
-        stop("Unable to connect to S3 bucket")
-      }
-      message("attempt ", i, " to connect to S3 bucket failed. Trying again...")
-      i <- i + 1
-      # waiting time between tries increases with the number of tries
-      Sys.sleep(i)
-    }
-    if (!bucket_ok) {
-      stop("Unable to connect to S3 bucket")
-    }
 
     status <- status(x)
     fingerprint <- get_file_fingerprint(x)
     part <- substring(fingerprint, 1, 4)
 
-    existing <- get_bucket(
+    get_bucket(
       bucket = base,
-      prefix = paste(project, part, sep = "/"),
-      max = Inf
-    )
-    existing <- existing[names(existing) == "Contents"] %>%
-      sapply("[[", "Key")
-    current <- existing[grepl(sprintf("%s.rds$", fingerprint), existing)]
+      prefix = paste(project, part, sep = "/")
+    ) %>%
+      map_chr("Key") -> existing
+    sprintf("%s/%s/.*/([[:xdigit:]]{40})\\.rds", project, part) %>%
+      gsub("\\1", existing) -> hashes
+    if (fingerprint %in% hashes) {
+      if (!isTRUE(overwrite)) {
+        return(fingerprint)
+      } else {
+        old <- existing[hashes == fingerprint]
+        backup <- paste0(
+          "abv/backup/", sha1(list(project, fingerprint, status, Sys.time()))
+        )
+        copy_object(
+          from_object = old[1], to_object = backup,
+          from_bucket = base, to_bucket = base
+        )
+        delete_object(old, bucket = base)
+      }
+    }
+
     filename <- sprintf(
       "%s/%s/%s/%s.rds", project, part, status, fingerprint
-    ) %>%
-      normalizePath(winslash = "/", mustWork = FALSE) %>%
-      gsub(pattern = "//", replacement = "/") %>%
-      gsub(pattern = "^/", replacement = "")
-
-    if (length(current) > 0) {
-      if (!overwrite) {
-        return(current)
-      }
-      delete_object(object = current, bucket = base)
-    }
+    )
 
     # try several times to write to S3 bucket
     # avoids errors due to time out
@@ -148,10 +129,18 @@ setMethod(
       # waiting time between tries increases with the number of tries
       Sys.sleep(i)
     }
+    if (fingerprint %in% hashes && isTRUE(overwrite)) {
+      if (!bucket_ok) {
+        copy_object(
+          from_object = backup, to_object = old[1],
+          from_bucket = base, to_bucket = base
+        )
+      }
+      delete_object(backup, bucket = base)
+    }
     if (!bucket_ok) {
       stop("Unable to write to S3 bucket")
     }
-
     return(filename)
   }
 )
