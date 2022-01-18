d48b23420414ffaae3d0a84ea6c013bba01db6b7
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-01-18 13:31:36 +0100

    make validObject() more silent

--- R/validOject.R
+++ R/validOject.R
@@ -22,68 +22,72 @@ setMethod(
           lapply(files, validObject, test = test, complete = complete)
         )
       )
-    } else {
-      # assume x is a file or directory when length(x) == 1
-      if (file_test("-d", object)) {
-        # handle a directory
-        files <- list.files(
-          path = object,
-          pattern = "\\.rd(s|a)$",
-          ignore.case = TRUE,
-          full.names = TRUE,
-          recursive = TRUE
-        )
-        if (length(files) == 0) {
-          stop("no matching files found")
-        }
-        return(validObject(files, test = test, complete = complete))
-      } else {
-        if (!file_test("-f", object)) {
-          # object is not a file
-          return(
-            methods::validObject(
-              object = object,
-              test = test,
-              complete = complete
-            )
-          )
-        }
-
-        # handle a file
-        if (grepl("\\.rds$", object)) {
-          objects <- list(readRDS(file = object))
-          names(objects) <- object
-        } else {
-          local_environment <- new.env()
-          load(object, envir = local_environment)
-          objects <- ls(envir = local_environment) %>%
-            lapply(read_object_environment, env = local_environment)
-          names(objects) <- ls(envir = local_environment)
-        }
-        # handles an empty file
-        if (length(objects) == 0) {
-          return(
-            data.frame(
-              Filename = character(0),
-              Object = character(0),
-              Valid = logical(0)
-            )
-          )
-        }
-        invalid <- sapply(
-          objects,
-          function(x) {
-            try(validObject(x, test = test, complete = complete)) %>%
-              inherits("try-error")
-          }
-        )
-        data.frame(
-          Filename = object,
-          Object = names(objects),
-          Valid = !invalid,
-          stringsAsFactors = FALSE
-        )
-      }
     }
+
+    # assume x is a file or directory when length(x) == 1
+    if (file_test("-d", object)) {
+      # handle a directory
+      files <- list.files(
+        path = object,
+        pattern = "\\.rd(s|a)$",
+        ignore.case = TRUE,
+        full.names = TRUE,
+        recursive = TRUE
+      )
+      if (length(files) == 0) {
+        stop("no matching files found")
+      }
+      return(validObject(files, test = test, complete = complete))
+    }
+
+    if (!file_test("-f", object)) {
+      # object is not a file
+      return(
+        methods::validObject(
+          object = object,
+          test = test,
+          complete = complete
+        )
+      )
+    }
+
+    # handle a file
+    if (grepl("\\.rds$", object)) {
+      objects <- list(readRDS(file = object))
+      names(objects) <- object
+    } else {
+      local_environment <- new.env()
+      load(object, envir = local_environment)
+      objects <- ls(envir = local_environment) %>%
+        lapply(read_object_environment, env = local_environment)
+      names(objects) <- ls(envir = local_environment)
+    }
+
+    # handles an empty file
+    if (length(objects) == 0) {
+      return(
+        data.frame(
+          Filename = character(0),
+          Object = character(0),
+          Valid = logical(0)
+        )
+      )
+    }
+    invalid <- sapply(
+      objects,
+      function(x) {
+        try(
+          validObject(x, test = test, complete = complete),
+          silent = TRUE
+        ) %>%
+          inherits("try-error")
+      }
+    )
+    data.frame(
+      Filename = object,
+      Object = names(objects),
+      Valid = !invalid,
+      stringsAsFactors = FALSE
+    )
   }
 )
