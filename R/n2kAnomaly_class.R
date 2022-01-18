0713260c6f8d3bbf2562ce22e56bc1f4f899e289
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-01-18 13:32:11 +0100

    use assert_that() instead of if(){ stop()}

--- R/n2kAnomaly_class.R
+++ R/n2kAnomaly_class.R
@@ -43,6 +43,11 @@ setValidity(
     assert_that(is_chartor(object@AnomalyType$Description))
     assert_that(is_chartor(object@AnomalyType$Fingerprint))
 
+    assert_that(
+      anyDuplicated(colnames(object@Anomaly)) == 0,
+      msg = "duplicated column names in Anomaly"
+    )
+
     assert_that(has_name(object@Anomaly, "AnomalyType"))
     assert_that(has_name(object@Anomaly, "Analysis"))
     assert_that(has_name(object@Anomaly, "Parameter"))
@@ -56,32 +61,28 @@ setValidity(
     antijoin_anomalytype <- object@Anomaly %>%
       anti_join(object@AnomalyType, by = c("AnomalyType" = "Fingerprint")) %>%
       nrow()
-    if (antijoin_anomalytype > 0) {
-      stop("Some Anomaly have no matching Fingerprint in 'AnomalyType'")
-    }
+    assert_that(
+      antijoin_anomalytype == 0,
+      msg = "Some Anomaly have no matching Fingerprint in 'AnomalyType'"
+    )
 
     antijoin_anomaly <- object@Anomaly %>%
       anti_join(object@ParameterEstimate, by = c("Analysis", "Parameter")) %>%
       nrow()
-    if (antijoin_anomaly > 0) {
-      stop(
+    assert_that(
+      antijoin_anomaly == 0, msg =
 "Mismatch on Analysis and Parameter between Anomaly and ParameterEstimate slot"
-      )
-    }
+    )
 
     anomalytype_duplicate <- object@AnomalyType %>%
       select(.data$Fingerprint) %>%
       anyDuplicated()
-    if (anomalytype_duplicate > 0) {
-      stop("Duplicated anomalytypes")
-    }
+    assert_that(anomalytype_duplicate == 0, msg = "Duplicated anomalytypes")
 
     anomaly_duplicate <- object@Anomaly %>%
       select(.data$Analysis, .data$AnomalyType, .data$Parameter) %>%
       anyDuplicated()
-    if (anomaly_duplicate > 0) {
-      stop("Duplicated anomalies")
-    }
+    assert_that(anomaly_duplicate == 0, msg = "Duplicated anomalies")
     return(TRUE)
   }
 )
