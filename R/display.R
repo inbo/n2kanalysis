75f418e210140103e3abdeda15305373092f93cb
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-01-24 16:50:40 +0100

    export the display function

--- R/display.R
+++ R/display.R
@@ -1,11 +1,16 @@
-#' @noRd
+#' Display a message.
+#'
+#' This is a short cut for `if(verbose) message(x)`.
+#' @export
 #' @importFrom utils flush.console
 #' @importFrom assertthat assert_that is.flag noNA
 display <- function(verbose, message, linefeed = TRUE) {
-  assert_that(is.flag(verbose))
-  assert_that(noNA(verbose))
+  assert_that(is.flag(verbose), noNA(verbose))
+  assert_that(is.flag(linefeed), noNA(linefeed))
+
   if (verbose) {
     message(message, appendLF = linefeed)
     flush.console()
   }
+  return(invisible(NULL))
 }
