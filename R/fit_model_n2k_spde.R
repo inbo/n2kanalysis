63a6909efd591bb2594aa55eb9d89945a06f4b1a
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-09-10 20:06:54 +0200

    add a method for fit_model() on an n2kSpde

---
+++ R/fit_model_n2k_spde.R
@@ -0,0 +1,159 @@
+#' @rdname fit_model
+#' @importFrom methods setMethod new
+#' @importFrom assertthat assert_that is.number
+#' @importMethodsFrom multimput impute
+#' @include n2k_spde_class.R
+#' @param timeout the optional number of second until the model will time out
+#' @inheritParams multimput::impute
+setMethod(
+  f = "fit_model",
+  signature = signature(x = "n2kSpde"),
+  definition = function(
+    x, status = "new", ..., timeout = NULL, seed = get_seed(x),
+    num_threads = NULL, parallel_configs = TRUE
+  ) {
+    assert_that(
+      requireNamespace("INLA", quietly = TRUE),
+      msg = "INLA package required but not installed."
+    )
+    validObject(x)
+    assert_that(is.character(status), length(status) >= 1)
+
+    # don't fit model when status doesn't match
+    if (!(status(x) %in% status)) {
+      return(x)
+    }
+
+    set.seed(seed)
+
+    data <- get_data(x)
+    matern <- spde2matern(x@Spde)
+    index <- INLA::inla.spde.make.index("matern_spde", n.spde = matern$n.spde)
+    x@AnalysisMetadata$formula |>
+      c("f(matern_spde, model = matern)") |>
+      paste(collapse = " + ") |>
+      as.formula() -> model_formula
+    fm <- terms(model_formula)
+    response <- all.vars(fm)[attr(fm, "response")]
+    stack_observed <- INLA::inla.stack(
+      data = data[!is.na(data[[response]]), response, drop = FALSE],
+      A = list(1, make_a(object = x@Spde, data[!is.na(data[[response]]),])),
+      tag = "observed",
+      effects = list(
+        data[
+          !is.na(data[[response]]), colnames(data) != response, drop = FALSE
+        ],
+        index
+      )
+    )
+    INLA::inla.stack(
+      data = data[is.na(data[[response]]), response, drop = FALSE],
+      A = list(1, make_a(object = x@Spde, data[is.na(data[[response]]),])),
+      tag = "observed",
+      effects = list(
+        data[is.na(data[[response]]), colnames(data) != response, drop = FALSE],
+        index
+      )
+    ) |>
+      INLA::inla.stack(stack_observed) -> stack_total
+    # prepare linear combinations
+    lc <- model2lincomb(x@LinearCombination)
+    # prepare inla() arguments
+    control <- x@Control
+    control$formula <- model_formula
+    control$family <- x@Family
+    # fit model
+    if (mean(is.na(data[[response]])) < 0.10) {
+      # directly fit model when less than 10% missing data
+      control$data <- INLA::inla.stack.data(stack_total, spde = spde)
+      control$control.predictor = list(
+        A = INLA::inla.stack.A(stack_total), link = 1
+      )
+      control$lincomb <- lc
+      model <- try({
+        if (!is.null(timeout)) {
+          assert_that(is.number(timeout), timeout > 0)
+          setTimeLimit(cpu = timeout, elapsed = timeout)
+        }
+        do.call(INLA::inla, control)
+      }, silent = TRUE)
+    } else {
+      # first fit model without missing data
+      control$data <- INLA::inla.stack.data(stack_observed, spde = spde)
+      control$control.predictor = list(A = INLA::inla.stack.A(stack_observed))
+      m0 <- try({
+        if (!is.null(timeout)) {
+          assert_that(is.number(timeout), timeout > 0)
+          setTimeLimit(cpu = timeout, elapsed = timeout)
+        }
+        do.call(INLA::inla, control)
+      }, silent = TRUE)
+      if (inherits(m0, "try-error")) {
+        status(x) <- ifelse(
+          grepl("time limit", m0), "time-out", "error"
+        )
+        return(x)
+      }
+      # then refit with missing data
+      control$data <- INLA::inla.stack.data(stack_total, spde = spde)
+      control$control.predictor = list(
+        A = INLA::inla.stack.A(stack_total), link = 1
+      )
+      control$lincomb <- lc
+      control$control.update <- list(result = m0)
+      model <- try({
+        if (!is.null(timeout)) {
+          assert_that(is.number(timeout), timeout > 0)
+          setTimeLimit(cpu = timeout, elapsed = timeout)
+        }
+        do.call(INLA::inla, control)
+      }, silent = TRUE)
+    }
+
+    # handle error in model fit
+    if (inherits(model, "try-error")) {
+      status(x) <- ifelse(
+        grepl("time limit", model), "time-out", "error"
+      )
+      return(x)
+    }
+    # return fitted model when no imputation is required
+    if (x@ImputationSize == 0) {
+      return(n2k_spde(data = x, model_fit = model, status = "converged"))
+    }
+
+    imputed <- try(impute(
+      model = model, n_imp = x@ImputationSize, minimum = x@Minimum,
+      seed = seed, num_threads = num_threads, extra = x@Extra,
+      parallel_configs = parallel_configs
+    ))
+    if (inherits(imputed, "try-error")) {
+      return(n2k_spde(data = x, model_fit = model, status = "error"))
+    }
+    # return fitted model with imputations
+    return(n2k_inla(
+      data = x, model_fit = model, status = "converged", raw_imputed = imputed
+    ))
+  }
+)
+
+model2lincomb <- function(lincomb) {
+  if (is.null(lincomb)) {
+    return(NULL)
+  }
+  if (inherits(lincomb, "matrix")) {
+    lincomb |>
+      as.data.frame() |>
+      as.list() %>%
+      INLA::inla.make.lincombs() |>
+      setNames(rownames(lincomb)) -> lc
+    return(lc)
+  }
+  lc <- INLA::inla.make.lincombs(lincomb)
+  if (is.matrix(lincomb[[1]])) {
+    names(lc) <- rownames(lincomb[[1]])
+  } else {
+    names(lc) <- names(lincomb[[1]])
+  }
+  return(lc)
+}
