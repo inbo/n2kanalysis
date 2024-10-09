124f8cd706ac926f09c31fb4b5cee71040384960
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-10-09 11:20:25 +0200

    export order_manifest()

--- R/manifest_yaml_to_bash.R
+++ R/manifest_yaml_to_bash.R
@@ -204,7 +204,15 @@ date",
   }
 )
 
+#' Convert a manifest in vector of analysis fingerprints
+#'
+#' The order of the analysis is determined by the parent-child relationship.
+#' It starts with the analyses without parents.
+#' Then it adds the analyses with parents that have already been added.
+#' This process is repeated until all analyses have been added.
+#' @inheritParams store_manifest
 #' @importFrom assertthat assert_that
+#' @export
 order_manifest <- function(manifest) {
   assert_that(inherits(manifest, "n2kManifest"))
   full <- slot(manifest, "Manifest")
